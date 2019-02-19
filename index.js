ebfc9137005e3c5e033e6a839146f337bb1fc9fb
Ryan Lewis <thefakeryan@icloud.com>
2019-02-19 13:39:40 -0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const dayjs = require('dayjs')
+
+module.exports = (start, end, direction = 'forward') => {
+  if (!start || !end) {
+    console.error('NEED START AND END DATE <YYYY-MM-DD>')
+    return false
+  }
+  const s = dayjs(start)
+  const e = dayjs(end)
+  const dateArr = []
+  const l = e.diff(s, 'day')
+  for (let i = 0; i <= l; i++) {
+    dateArr.push(s.add(i, 'day').format('YYYY-MM-DD'))
+  }
+  switch (direction) {
+    case 'backward':
+      return dateArr.reverse()
+    default:
+      return dateArr
+  }
+}
---
+++ run.js
@@ -0,0 +1,5 @@
+const dateArr = require('./index')
+
+const x = dateArr('2019-01-11', '2019-02-11')
+
+console.info(x)
